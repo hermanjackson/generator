16d61234bf9e2f30ef8644d07508bc896f73d119
hermanjackson <hermanjackson561@gmail.com>
2021-03-09 00:35:43 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#project1").innerHTML = generateexcuse();
+  console.log();
+};
+let generateexcuse = function() {
+  let who = ["we", "they"];
+  let how = ["build", "helped", "guided"];
+  let what = ["websites", "each other"];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let howindex = Math.floor(Math.random() * how.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+
+  return who[whoindex] + "" + how[howindex] + "" + what[whatindex];
 };
